5a394d7f16b649fc63fae6b38e244d77eee845da
Maxim Pokrovsky <maxim@soundpress.biz>
2021-09-02 01:07:04 +0300

    Disqus embedded

--- resources/views/orgproject.blade.php
+++ resources/views/orgproject.blade.php
@@ -133,17 +133,25 @@
                 </div>
 
                 <div class="my-3">
-					<div id="hashover"></div>
-					<script type="text/javascript">
-						var rows="2";        // Sets "Comments" field height
-						var name_on="yes";    // Disables "Name" field
-						var passwd_on="no";  // Disables "Password" field
-						var email_on="no";   // Disables "E-mail" field
-						var sites_on="no";   // Disables "Website" field
-						var icon_size=24;
+					<div id="disqus_thread"></div>
+					<script>
+						/**
+						*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+						*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+						/*
+						var disqus_config = function () {
+						this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+						this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+						};
+						*/
+						(function() { // DON'T EDIT BELOW THIS LINE
+						var d = document, s = d.createElement('script');
+						s.src = 'https://databook-wegov-nyc.disqus.com/embed.js';
+						s.setAttribute('data-timestamp', +new Date());
+						(d.head || d.body).appendChild(s);
+						})();
 					</script>
-					<script type="text/javascript" src="/hashover/comments.php"></script>
-					<noscript>You must have JavaScript enabled to use the comments.</noscript>
+					<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>				
                 </div>
 			</div>
 
