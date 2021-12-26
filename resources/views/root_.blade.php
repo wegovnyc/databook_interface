ca10c90b884dc4be49983efcd289da0d340f5700
Maxim Pokrovsky <maxim@soundpress.biz>
2021-12-26 16:19:59 +0200

    Civil Services Titles - root page tweaked - slide 87

--- resources/views/root_.blade.php
+++ resources/views/root_.blade.php
@@ -9,76 +9,50 @@
 @section('content')
 <div class="inner_container">
 	<div class="jumbotron">
-        <div class="col-md-5 home_bgcontent">
+        <div class="col-md-7 home_bgcontent">
             <h1>WeGovNYC DataBook</h1>
             <p>We collect and join datasets together to create a data-driven view of New York City government.</p>
+            <a class="btn_org_home mr-3" href="{{ route('orgs') }}" role="button">Agency Profiles</a>
+			<a class="btn_org_home mr-3" href="{{ route('districts') }}" role="button">District Profiles</a>
+            <a class="btn_org_home" href="{{ route('projects') }}" role="button">Capital Projects Profiles</a>
         </div>
     </div>
     <div class="homeround_content">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-4">
 				<div class="circle_img">
 					<img src="/img/profile.png" title="" alt="">
 				</div>
 				<div class="content_area">
-					<h4>Organizations</h4>
-					<p>City agencies and related group, integrating dozens of datasets.</p>
-					<a class="btn_org_home" href="{{ route('orgs') }}" role="button">Organizations Profiles</a>
-
+					<h4>Profiles</h4>
+					<p>Organization's descriptions, contact and social media, ratings and more.</p>
 				</div>
             </div>
-            <div class="col-md-6">
-				<div class="circle_img">
-					<img src="/img/projects.png" title="" alt="">
-				</div>
-				<div class="content_area">
-					<h4>Projects</h4>
-					<p>Profiles with budget and timelines for all city capital project.</p>
-					<a class="btn_org_home" href="{{ route('projects') }}" role="button">Capital Projects Profiles</a>
-				</div>
-            </div>
-            <div class="col-md-6">
-				<div class="circle_img">
-					<img src="/img/indicators.png" title="" alt="">
-				</div>
-				<div class="content_area">
-					<h4>Districts</h4>
-					<p>Neighborhood, city council & community district data.</p>
-					<a class="btn_org_home" href="{{ route('districts') }}" role="button">District Profiles</a>
-				</div>
-            </div>
-            <div class="col-md-6">
-				<div class="circle_img">
-					<img src="/img/services.png" title="" alt="">
-				</div>
-				<div class="content_area">
-					<h4>Civil Service</h4>
-					<p>Job titles, positions, people, contacts, salaries and more.</p>
-					<a class="btn_org_home" href="{{ route('titles') }}" role="button">Civil Service Titles</a>
-				</div>
-            </div>
-			
-            <div class="col-md-12 mb-3">
-				<div class="content_area">
-					<h4 class="mb-1">How it Works</h4>
-					<p>Our data pipeline takes NYC open data, normalizes it and publishes it as bulk data and via Carto API. <a href="https://www.notion.so/wegovnyc/DataBook-c44e74c262a84b67b7aabb14885e1ec6" target="_blank">Learn More</a></p>
-				</div>
-            </div>
-            <div class="col-md-12">
-				<div class="content_area">
-					<h4 class="mb-1">Work with Us</h4>
-					<p>We create information products and experience for elected officials, journalists, educators, city agencies and others. <a href="https://wegovnyc.notion.site/Contact-Us-54b075fa86ec47ebae48dae1595afc2c" target="_blank">Contacts Us</a></p>
-				</div>
-            </div>
-			
-			{{--
-            <div class="col-md-6">
+            <div class="col-md-4">
 				<div class="circle_img">
 					<img src="/img/budget.png" title="" alt="">
 				</div>
 				<div class="content_area">
 					<h4>Budgets</h4>
 					<p>Line items budgets for each agency and how they change over time.</p>
+				</div>
+            </div>
+            <div class="col-md-4">
+				<div class="circle_img">
+					<img src="/img/services.png" title="" alt="">
+				</div>
+				<div class="content_area">
+					<h4>Services</h4>
+					<p>Health, human and social media, ratings and more.</p>
+				</div>
+            </div>
+            <div class="col-md-4">
+				<div class="circle_img">
+					<img src="/img/projects.png" title="" alt="">
+				</div>
+				<div class="content_area">
+					<h4>Projects</h4>
+					<p>Capital projects managed by city agencies.</p>
 				</div>
             </div>
             <div class="col-md-4">
@@ -117,14 +91,20 @@
 					<p>Formal requests from community boards of city agencies.</p>
 				</div>
             </div>
-			--}}
+            <div class="col-md-4">
+				<div class="circle_img">
+					<img src="/img/indicators.png" title="" alt="">
+				</div>
+				<div class="content_area">
+					<h4>Indicators</h4>
+					<p>Key performance indicators (KPIs) for agencies and geographies.</p>
+				</div>
+            </div>
         </div>
-		{{--
         <div class="text-center bottom_text col-md-12">
             <h3>We’re adding data and organizing collaborations all the time.</h3>
             <a href="https://www.notion.so/wegovnyc/DataBook-c44e74c262a84b67b7aabb14885e1ec6" class="learn_more" target="_blank">Learn More</a>
         </div>
-		--}}
     </div>
 </div>
 @endsection
