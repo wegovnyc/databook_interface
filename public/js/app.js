26ed288cae2c9c9258a23514e99b34abc6f6a8ce
d9it <d9ithub@gmail.com>
2022-01-25 12:44:51 +0530

    notice page design done

--- public/js/app.js
+++ public/js/app.js
@@ -19431,8 +19431,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\xampp\htdocs\devin_git\databook_wegov\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\xampp\htdocs\devin_git\databook_wegov\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\xampp\htdocs\devin_git\databook_interface\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\xampp\htdocs\devin_git\databook_interface\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
