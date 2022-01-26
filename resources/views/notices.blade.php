2a17411f9681605e231ecb431fa448a408e533dc
Maxim Pokrovsky <maxim@soundpress.biz>
2022-01-26 08:47:09 +0200

    Notices issue, notices new page design

--- resources/views/notices.blade.php
+++ resources/views/notices.blade.php
@@ -535,7 +535,7 @@
 					  @endphp
 					<div class="card">
 						<a href="{!! $a['URL'] !!}" target="_blank" class="hoveronly">
-							@if ($img[0]['thumbnails']['large']['url'])
+							@if ($img[0]['thumbnails']['large']['url'] ?? null)
 								<div style="height: 250px; overflow: hidden; display: block; margin: 20px; text-align: center;    background: #f0f0f0;">
 									<img src="{{ $img[0]['thumbnails']['large']['url'] }}" alt="{{ $a['Title'] }}" style="max-width: 100%; max-height: 100%;width:auto; margin: 0 auto;">
 								</div>
